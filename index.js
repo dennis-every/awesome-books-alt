8de64b253e2e363f65147e392a602115df79c888
eNeville <17412822+eNeville@users.noreply.github.com>
2023-01-09 17:16:32 -0400

    Add addBook functionality

--- index.js
+++ index.js
@@ -1,38 +1,52 @@
 const booksList = document.getElementById('books_list');
+const bookForm = document.getElementById('booksForm');
 
 function Book(title, author, id) {
-    this.title = title;
-    this.author = author;
-    this.id = id;
+  this.title = title;
+  this.author = author;
+  this.id = id;
 }
 
 const booksArray = [
-    {
-        title: "Lord of the Rings",
-        author: "Tolkein",
-        id: "0"
-    },
-    {
-        title: "Harry Potter",
-        author: "Author",
-        id: "1"
-    }
+  {
+    title: 'Lord of the Rings',
+    author: 'Tolkein',
+    id: '0',
+  },
+  {
+    title: 'Harry Potter',
+    author: 'Author',
+    id: '1',
+  },
 ];
 
+const appendBook = (title, author, id) => {
+  const bookItem = document.createElement('li');
+  bookItem.innerHTML = `
+    <h2>${title}</h2>
+    <h2>${author}</h2>
+    <button type="button" id=${id}>Remove</button>`;
+  booksList.append(bookItem);
+};
 
 function loadBooks() {
-    booksArray.forEach((book) => {
-        const bookItem = document.createElement('li');
-        bookItem.innerHTML = `<h2>${book.title}</h2>
-                              <h2>${book.author}</h2>
-                              <button type="button" id=${book.id}>Remove</button>`
-        booksList.append(bookItem);
-    })
-};
-
+  booksArray.forEach((book) => {
+    appendBook(book.title, book.author, book.id);
+  });
+}
 
 window.onload = (event) => {
-    loadBooks();
+  loadBooks();
 };
 
-
+bookForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const book = new Book(
+    bookForm.elements['title'].value,
+    bookForm.elements['author'].value,
+    Math.random()
+  );
+  booksArray.push(book);
+  appendBook(book.title, book.author, book.id);
+  bookForm.reset();
+});
