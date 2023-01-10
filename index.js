2a6b2d0b3c54f6dd822d7cbb6e6b099b0bab34e3
jmonto55 <jmonto55@gmail.com>
2023-01-09 19:55:19 -0500

    ESLint debugging #3

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function Book(title, author, id) {
 
 let booksArray = [];
 
-// let availableStorage;
+let availableStorage;
 
 function retrieveData() {
   const data = availableStorage.getItem('books');
@@ -69,40 +69,40 @@ window.onload = () => {
 };
 
 // Check if local storage available
-// function storageAvailable(type) {
-//   let storage;
-//   try {
-//     storage = window[type];
-//     const x = '__storage_test__';
-//     storage.setItem(x, x);
-//     storage.removeItem(x);
-//     return true;
-//   } catch (e) {
-//     return (
-//       e instanceof DOMException &&
-//       // everything except Firefox
-//       (e.code === 22 ||
-//         // Firefox
-//         e.code === 1014 ||
-//         // test name field too, because code might not be present
-//         // everything except Firefox
-//         e.name === 'QuotaExceededError' ||
-//         // Firefox
-//         e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-//       // acknowledge QuotaExceededError only if there's something already stored
-//       storage &&
-//       storage.length !== 0
-//     );
-//   }
-// }
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === 'QuotaExceededError' ||
+        // Firefox
+        e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
+  }
+}
 
-// if (storageAvailable('localStorage')) {
-//   // Yippee! We can use localStorage awesomeness
-//   availableStorage = window.localStorage;
-// } else {
-//   // Too bad, no localStorage for us
-//   availableStorage = null;
-// }
+if (storageAvailable('localStorage')) {
+  // Yippee! We can use localStorage awesomeness
+  availableStorage = window.localStorage;
+} else {
+  // Too bad, no localStorage for us
+  availableStorage = null;
+}
 
 bookForm.addEventListener('submit', (event) => {
   event.preventDefault();
